2a909e8fd1e3de83a889747ecd3133f9d87262aa
markmumba <markmumba01@gmail.com>
2019-08-25 12:55:47 +0300

    added the app and project

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+import datetime as dt
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title=models.CharField(max_length=60)
+    categories = models.ManyToManyField(categories)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    
+    post_date = models.DateTimeField(auto_now_add=True)
