cf58ff8661262ea0c4f8d44ccc84f524bed1ed85
markmumba <markmumba01@gmail.com>
2019-08-26 11:54:28 +0300

    Deployment

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import django_heroku
 import dj_database_url
-from decouple import config,Csv
+from decouple import Config,Csv
 
 
 
